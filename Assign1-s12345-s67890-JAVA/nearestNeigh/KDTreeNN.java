ba6505c21f92d34048a734051ddb5f8b4ad1133a
Ahmed <s3813842@student.rmit.edu.au>
2021-08-21 23:44:20 +1000

    Added isPointIn implementation
    added isPointIn implementation and initial KDNode class

--- Assign1-s12345-s67890-JAVA/nearestNeigh/KDTreeNN.java
+++ Assign1-s12345-s67890-JAVA/nearestNeigh/KDTreeNN.java
@@ -9,34 +9,119 @@ import java.util.List;
  * 
  */
 public class KDTreeNN implements NearestNeigh{
-
-    @Override
+	
+	List<Point> pointList;
+    
+	KDNode root; 
+	
+	@Override
     public void buildIndex(List<Point> points) {
-        // To be implemented.
+        for (Point p: points)
+        { pointList.add(p);}
+        
+        //build a tree
     }
 
     @Override
     public List<Point> search(Point searchTerm, int k) {
-        // To be implemented.
+       //get search term and find k amount of nearest neighbours
+    	//implemnt KD tree
+
         return new ArrayList<Point>();
     }
 
     @Override
     public boolean addPoint(Point point) {
-        // To be implemented.
-        return false;
+        //REVISE THIS
+    	if (pointList.contains(point)){
+    		return false;
+    	}
+    	
+    	KDNode currentNode = root;
+    	if (root == null) {
+            root = new KDNode(point, null, null, null);
+        //2nd depth
+    	} else if (point.lat < root.point.lon) {
+            root.left = new KDNode(point, root, null,null);
+        } else {
+            root.right = new KDNode(point, root, null,null);;
+        }
+
+    
+    	return true;
     }
 
     @Override
     public boolean deletePoint(Point point) {
-        // To be implemented.
-        return false;
+    	if (pointList.contains(point)){
+    		pointList.remove(point);
+    		return true;
+    	}
+    	return false;
     }
 
     @Override
     public boolean isPointIn(Point point) {
-        // To be implemented.
+
+        boolean isLatitude = true;
+        KDNode newNode = new KDNode(point, null,null,null);
+        KDNode currentNode = root;
+       // if current node is null return false
+        while (currentNode != null) {
+        	if (currentNode.equals(newNode)) {return true;}
+        	
+        	if (isLatitude == true)
+        	{ 
+        		if (newNode.point.lat < currentNode.point.lat) {
+        			currentNode = currentNode.getLeftChild(currentNode);
+        			isLatitude = false;
+        		}else
+        		{
+        				currentNode = currentNode.getRightChild(currentNode);
+        				isLatitude = false;
+        		}
+        	}else 
+        		{ 
+        			if (newNode.point.lon < currentNode.point.lon)
+        			{
+        				currentNode = currentNode.getLeftChild(currentNode);
+        				isLatitude = true;
+        			}else 
+        			{
+        			currentNode = currentNode.getRightChild(currentNode);
+        			isLatitude = false;
+        			}
+        		}
+        }
         return false;
     }
+}
+
+//KD NODE CLASS
+class KDNode {
+    Point point;
+    KDNode parent;
+    KDNode leftChild;
+    KDNode rightChild;
+    
+    public KDNode (Point point, KDNode parent, KDNode leftChild, KDNode rightChild) {
+    	this.point = point;
+    	this.parent = parent;
+    	this.leftChild = leftChild;
+    	this.rightChild = rightChild;
+    }
+    
+
+	public KDNode getLeftChild(KDNode parent)
+    {
+    	this.parent = parent;
+    	return parent.leftChild;
+    }    
+	
+	public KDNode getRightChild(KDNode parent) {
+    	this.parent = parent;
+    	return parent.rightChild;
+		
+	}
 
 }
